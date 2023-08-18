cc489159c110a9a2df8e59e1538007dc0fef4c3d
tobias <tobiasnaltamirano2003@gmail.com>
2023-08-18 13:54:13 -0300

    primer commit

---
+++ hola.js
@@ -0,0 +1,15 @@
+// Crear un archivo llamado hola.js que muestre por consola un “Hola Mundo”
+// Ejecutar node hola.js muestre el mensaje “Hola Mundo”
+// Crear otro archivo llamado “Calculos.js”
+// Crear una función que reciba dos números y retorne la suma de ellos
+// Crear una función que reciba dos números y devuelva el resultado entre la división del primero sobre el segundo
+// Crear una función que reciba un Array de N elementos y devuelva el mayor valor de ese array
+// Hacer uso del console.log para mostrar la suma de 5 y 10 utilizando las funciones creadas
+// Hacer uso del console.log para mostrar la suma la división de 20 y 2 (que sucede si envió 20 y 0)
+// Hacer uso del console.log para mostrar el mayor de la lista [2,8,9,7,5,6]
+// Ejecutar node Calculos.js para ver los resultados
+
+// Subir el codigo a un repositorio github y compartir el link.
+
+console.log("Hola Mundo");
+
